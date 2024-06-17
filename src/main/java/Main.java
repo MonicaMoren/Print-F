4ada3101ccbebeab9bb3f35593d653db8f41d46c
Monica Moreno <169068483+MonicaMoren@users.noreply.github.com>
2024-06-17 12:39:31 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+  public static void main(String[] args) {
+   /* int Número = 15 ;
+    String nome = "Monica";
+System.out.println ("O meu nome é:" + " " + nome + " " + "Minha Idade é: " + " " + " " + Número);
+    System.out.printf("Minha Idade é: %d ", Número );
+    */
+
+   //String nome = "Monica";
+   // int Dia = 9;
+  // int Mês = 2; 
+  // int Ano = 2009;
+  double Pi = 3.141592;
+System.out.printf("O valor de pi é: %.2f ",Pi);
+    
+   // System.out.printf("Meu Aniversario é: %d Dia %d Mês %d Ano",Dia, Mês, Ano);
+
+    // %d numeros inteiros
+    // %f numerios com virgula
+  }
+}
